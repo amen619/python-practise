13a4000c3ac1a7c66f424d48b5a815caa6aadff4
nived <nivedvelayudhan1508@gmail.com>
2020-03-29 21:50:56 +0530

    Added spaces

--- operators.py
+++ operators.py
@@ -4,12 +4,18 @@
 
 print('Hello World!!')
 
+# You can also use this function to put a blank line on the screen; just call print() with nothing in between the parentheses.
+
+print()
+
 print('Whats your name?')
 name = input()
 
+print()
 print('Whats your age ' + name + '?')
 age = input()
 
+print()
 print('You are '+ name + ' and your age is ' + age)
 
 
