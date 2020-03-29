934be8ab04a361dcac1411e9a461d0da6aa23da5
nived <nivedvelayudhan1508@gmail.com>
2020-03-29 20:51:16 +0530

    string and interger variables

---
+++ operators.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.6
+
+# Print Hello World and ask for name
+
+print('Hello World!!')
+
+print('Whats your name?')
+name = input()
+
+print('Whats your age ' + name + '?')
+age = input()
+
+print('You are '+ name + ' and your age is ' + age)
+
+
